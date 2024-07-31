415622297f8aee241ea4fe2ba1c4c6162f0f9a3e
Jhoony <webwork.jhoony@gmail.com>
2024-07-31 10:33:15 +0700

    fix

--- src/entity/BooksRepository.ts
+++ src/entity/BooksRepository.ts
@@ -7,13 +7,6 @@ class BookRepository {
   id: string
   books: Array<{ name: string; id: string }>
 
-  constructor() {
-    this.books = [
-      { name: 'hello', id: '1' },
-      { name: 'hi', id: '2' }
-    ]
-  }
-
   async getBookById(id: string): Promise<object | undefined> {
     return await Book.findById(id).select('-__v -mimetype')
   }
